490a34c13201d444f3e2ce8bce163722cac5cfa9
Hollow <91344624+massivenobody@users.noreply.github.com>
2025-04-07 10:51:50 -0500

    feat: add header with github logo and title
    - Add GitHub logo SVG to assets
    - Create header with logo and app title

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
+import githubLogo from './assets/github.svg'
 
 function App() {
 
   return (
     <>
+      <div className="title">
+        <img src={githubLogo} alt="GitHub Logo" />
+        <h1>Get Random GitHub Repo</h1>
+      </div>
     </>
   )
 }
