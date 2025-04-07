dca654d6b0102afae3232e6811be60f53cbb25ac
Hollow <91344624+massivenobody@users.noreply.github.com>
2025-04-07 10:54:36 -0500

    feat: create repofetcher component
    - Add blank RepoFetcher component
    - Import and integrate into App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import githubLogo from './assets/github.svg'
+import RepoFetcher from './components/RepoFetcher'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <div className="title">
         <img src={githubLogo} alt="GitHub Logo" />
         <h1>Get Random GitHub Repo</h1>
+        <RepoFetcher />
       </div>
     </>
   )
---
+++ src/components/RepoFetcher.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RepoFetcher: React.FC = () => {
+  return (
+    <div className="repo-fetcher">
+      
+    </div>
+  )
+}
+
+export default RepoFetcher 
