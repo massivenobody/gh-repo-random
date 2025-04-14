02caef05a4767064f76c4507ba2aa17fd1c9d06d
Hollow <91344624+massivenobody@users.noreply.github.com>
2025-04-14 14:23:27 -0500

    refactor: improve languages data handling
    - Fix languages variable name typo
    - Add filtering for duplicate languages
    - Remove empty language entries

--- src/data/languages.ts
+++ src/data/languages.ts
@@ -1,8 +1,13 @@
-const languagues = await fetch('https://raw.githubusercontent.com/kamranahmedse/githunt/master/src/components/filters/language-filter/languages.json')
+import { LanguageOption } from "@/types/language";
+
+const languages = await fetch('https://raw.githubusercontent.com/kamranahmedse/githunt/master/src/components/filters/language-filter/languages.json')
     .then(res => res.json())
     .then(data => data.map((language: {title: string, value: string}) => ({
         label: language.title,
         value: language.value,
-    })));
+    }))).then(languages => languages.filter((language: LanguageOption, index: number, self: LanguageOption[]) => 
+        index === self.findIndex((l) => l.value === language.value)
+            && language.value !== ""
+    ));
 
-export default languagues;
+export default languages;
